374f0d7752586b479bda4b5bfa988ab4da54b5bb
Sala D10 <CSalaD10@uniboyaca.edu.co>
2024-04-20 09:56:16 -0500

    Ejercicio terminado

--- Ejercicio_3_sudoku/sources/js/sudoku.js
+++ Ejercicio_3_sudoku/sources/js/sudoku.js
@@ -3,13 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnResolver=document.getElementById("btn-resolver");
     btnResolver.addEventListener('click',resolverSudoku);
 
-    const tablaSudoku = document.getElementById("tabla-sudoku");
-    const mediaCuadratica = 9;
+    const btnReiniciar = document.getElementById("btn-reiniciar");
+    btnReiniciar.addEventListener('click', reiniciarSudoku);
 
-    for (let fila = 0; fila < mediaCuadratica; fila++) {
+
+    const tablaSudoku = document.getElementById("tabla-sudoku");
+    const Cuadricula = 9;
+
+    for (let fila = 0; fila < Cuadricula; fila++) {
         const nuevaFila = document.createElement("tr");
 
-        for (let colum = 0; colum < mediaCuadratica; colum++) {
+        for (let colum = 0; colum < Cuadricula; colum++) {
             const celda = document.createElement("td");
             const input = document.createElement("input");
 
@@ -17,22 +21,44 @@ document.addEventListener('DOMContentLoaded', function () {
             input.className = "celda";
             input.id = `celda-${fila}-${colum}`;
 
+            //Validacion en el momento
+            input.addEventListener('input', function(event){
+                validarEntrada(event,fila,colum);
+            });
+            //-------------------------------------
+
             celda.appendChild(input);
             nuevaFila.appendChild(celda);
         }
         tablaSudoku.appendChild(nuevaFila);
     }
+
+    //Función para reiniciar el juego
+
+    function reiniciarSudoku(){
+        for(let fila = 0; fila < Cuadricula; fila++){
+            for(let col = 0; col < Cuadricula; col++){
+                const celdaId = `celda-${fila}-${col}`;
+                const celda = document.getElementById(celdaId);
+                celda.value="";
+                celda.classList.remove("resolverEfecto", "entradaUsuario");
+            }
+        }
+    }
+
+   
 });
+
 
 //Funcion de Resolver
 
 async function resolverSudoku(){
-    const mediaCuadratica = 9;
+    const Cuadricula = 9;
     const listaSudoku=[];
 
-    for (let fila = 0; fila < mediaCuadratica; fila++) {
+    for (let fila = 0; fila < Cuadricula; fila++) {
         listaSudoku[fila]=[];
-        for (let colum = 0; colum < mediaCuadratica; colum++) {
+        for (let colum = 0; colum < Cuadricula; colum++) {
             const celdaId = `celda-${fila}-${colum}`;
             const celdaValor=document.getElementById(celdaId).value;
             listaSudoku[fila][colum]=celdaValor !== ""? parseInt(celdaValor):0;
@@ -41,8 +67,8 @@ async function resolverSudoku(){
 
     //identificador de celdas ingresadas
 
-    for (let fila = 0; fila < mediaCuadratica; fila++) {
-        for (let colum = 0; colum < mediaCuadratica; colum++) {
+    for (let fila = 0; fila < Cuadricula; fila++) {
+        for (let colum = 0; colum < Cuadricula; colum++) {
             const celdaId = `celda-${fila}-${colum}`;
             const celda=document.getElementById(celdaId);
 
@@ -54,8 +80,8 @@ async function resolverSudoku(){
 
     //Mostrar solucion 
     if(sudoku(listaSudoku)){
-        for(let fila=0; fila<mediaCuadratica;fila++){
-            for(let colum=0;colum<mediaCuadratica;colum++){
+        for(let fila=0; fila<Cuadricula;fila++){
+            for(let colum=0;colum<Cuadricula;colum++){
 
                 const celdaId=`celda-${fila}-${colum}`;
                 const celda=document.getElementById(celdaId);
@@ -74,14 +100,40 @@ async function resolverSudoku(){
 
 }
 
+//Función Juego Sudoku - Solucionador
+function juegoSudoku(tablero){
+    const Cuadricula = 9
+
+    for(let fila = 0; fila < Cuadricula; fila++){
+        for(let colum = 0; colum < Cuadricula; colum++){
+            if(tablero[fila][colum]===0){
+                for(let num = 1; num <= 9; num++){
+                    if(verificaConflictos(tablero,fila,colum,num)){
+                        tablero[fila][colum] = num;
+
+                        //Intentamos resolverlo con recursividad
+                        if(juegoSudoku(tablero)){
+                            return true;
+                        }
+                        tablero[fila][colum] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 
 //validacion de datos
-function validacionDatos(tabla,fila,colum,num){
-    const mediaCuadratica =9
+function validacionDatos(tablero,fila,colum,num){
+    const Cuadricula =9
 
     //verificacion fila x colum
-    for (let i = 0; i < mediaCuadratica; i++) {
-        if (tabla[fila][colum][i] == num || tabla[i][colum]==num) {
+    for (let i = 0; i < Cuadricula; i++) {
+        if (tablero[fila][colum][i] == num || tablero[i][colum]==num) {
             return false;
         }
     }
@@ -92,7 +144,7 @@ function validacionDatos(tabla,fila,colum,num){
 
     for (let i=filaPrimera; i<filaPrimera + 3; i++) {
         for (let j=columPrimera; j< columPrimera + 3; j++) {
-            if (tabla[i][j]==num) {
+            if (tablero[i][j]==num) {
                 return false;
             }  
         }
@@ -101,22 +153,22 @@ function validacionDatos(tabla,fila,colum,num){
 }
 
 // Solucionador
-function sudoku(tabla){
-    const mediaCuadratica=9
+function sudoku(tablero){
+    const Cuadricula=9
     
-    for(let fila=0;fila<mediaCuadratica;fila++){
-        for(let colum= 0;colum<mediaCuadratica; colum++){
+    for(let fila=0;fila<Cuadricula;fila++){
+        for(let colum= 0;colum<Cuadricula; colum++){
 
-            if (tabla[fila][colum]===0) {
+            if (tablero[fila][colum]===0) {
                 for(let num=1;num<=9;num++){
 
-                    if(validacionDatos(tabla,fila,colum,num)){
-                        tabla[fila][colum]=num;
+                    if(validacionDatos(tablero,fila,colum,num)){
+                        tablero[fila][colum]=num;
                         //recursividad
-                        if(sudoku(tabla)){
+                        if(sudoku(tablero)){
                             return true;
                         }
-                        tabla[colum][fila]=0;
+                        tablero[colum][fila]=0;
                     }
                 }
                 return false;
@@ -126,11 +178,107 @@ function sudoku(tabla){
     return true;
 }
 
+//verificacion para evitar errores en la solución
+function verificaConflictos(tablero, fila, colum, num){
+    const Cuadricula = 9
+
+    //Verificamos la fila y la columna
+    for(let i=0;i< Cuadricula;i++){
+        if(tablero[fila][i]===num || tablero[i][colum] === num){
+            return false;
+        }
+    }
+    //Verificacion  subcuadricula 3x3
+    const filaInicio = Math.floor(fila/3)*3;
+    const colInicio = Math.floor(colum/3)*3;
+
+    for(let i=filaInicio;i<filaInicio+3;i++){
+        for(let j=colInicio;j<colInicio+3;j++){
+            if(tablero[i][j] === num){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 //Efecto bonito de llenado
-
 function efectoLLenado(ef){
     return new Promise (sudoku=>setTimeout(sudoku,ef))
 }
 
 
+
+
+
+//Función para validar la entrada
+
+function validarEntrada(event,fila,colum){
+    const celdaId = `celda-${fila}-${colum}`;
+    const celda = document.getElementById(celdaId);
+    const valor = celda.value;
+
+    //Validar número del 1-9
+    if(!/^[1-9]$/.test(valor)){
+        //captura el error y muestra mensaje
+        Swal.fire({
+            icon: "warning", title: "["+valor+"] no es válido, ingrese otro numero (1-9)",
+            showConfirmButton: false,
+            timer: 2500
+          });
+          
+        celda.value = "";
+        return;
+    }
+    //Verificacion de numero ya existente
+    const numeroIngresado = parseInt(valor);
+
+//verificacion fila
+
+    for(let i=0;i<9;i++){
+        if(i !== colum && document.getElementById(`celda-${fila}-${i}`).value == numeroIngresado){
+            Swal.fire({
+                icon: "warning",
+                title: "["+numeroIngresado+"], ya esta en la Fila.",
+                showConfirmButton: false,
+                timer: 2500
+              });
+              
+            celda.value = "";
+            return;
+        }
+
+//verificacion columna
+
+        if(i!==fila && document.getElementById(`celda-${i}-${colum}`).value == numeroIngresado){
+            Swal.fire({
+                icon: "warning",title: "["+numeroIngresado+"], ya esta en la Columna.",
+                showConfirmButton: false,
+                timer: 2500
+              });
+              
+            celda.value = "";
+            return;
+        }
+    }
+
+//Verificar en la subcuadricula 3x3
+
+    const subcuadriculaFilaInicio = Math.floor(fila / 3) * 3;
+    const subcuadriculaColInicio = Math.floor(colum / 3) * 3;
+
+    for(let i = subcuadriculaFilaInicio; i < subcuadriculaFilaInicio + 3; i++){
+        for(let j = subcuadriculaColInicio; j < subcuadriculaColInicio + 3; j++){
+            if(i !== fila & j !== col && document.getElementById(`celda-${i}-${j}`).value == numeroIngresado){
+                Swal.fire({
+                    icon: "warning",title: "["+numeroIngresado+"], ya esta en la misma subcuadricula 3x3.",
+                    showConfirmButton: false,
+                    timer: 2500
+                  });
+                  
+                celda.value = "";
+                return;
+            }
+        }
+    }
+}
--- Ejercico 4/js/funtions.js
+++ Ejercico 4/js/funtions.js
@@ -3,7 +3,7 @@ function id( str ){
 }
 
 function obtener_valor( num_min, num_max ){
-    const amplitud_valores = num_max - num_min; //valor más alto - valor más bajo del random... (7 - 0)
-    const valor_al_azar = Math.floor( Math.random( ) * amplitud_valores ) + num_min; /* 5 - 15 = 10 + 5 */
+    const amplitud_valores = num_max - num_min; 
+    const valor_al_azar = Math.floor( Math.random( ) * amplitud_valores ) + num_min; 
     return valor_al_azar;
 }
